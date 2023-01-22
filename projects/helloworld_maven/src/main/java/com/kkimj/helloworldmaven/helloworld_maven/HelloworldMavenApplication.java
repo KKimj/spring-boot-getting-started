9570f03fe81282a632092ceae967a27a26213169
Jiun Kim <kkimj@hanyang.ac.kr>
2023-01-22 20:51:48 +0900

    Update to add `Hello World Maven`

---
+++ projects/helloworld_maven/src/main/java/com/kkimj/helloworldmaven/helloworld_maven/HelloworldMavenApplication.java
@@ -0,0 +1,13 @@
+package com.kkimj.helloworldmaven.helloworld_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldMavenApplication.class, args);
+	}
+
+}
---
+++ projects/helloworld_maven/src/test/java/com/kkimj/helloworldmaven/helloworld_maven/HelloworldMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkimj.helloworldmaven.helloworld_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
