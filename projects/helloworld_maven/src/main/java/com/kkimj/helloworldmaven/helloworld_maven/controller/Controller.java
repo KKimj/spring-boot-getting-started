3cad1af95f966a3539aebecbb9a23207b8cd8a63
Jiun Kim <40026920+KKimj@users.noreply.github.com>
2023-01-22 20:52:25 +0900

    Update to add `Hello World Maven` (#1)

---
+++ projects/helloworld_maven/src/main/java/com/kkimj/helloworldmaven/helloworld_maven/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.kkimj.helloworldmaven.helloworld_maven.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String hello_world() {
+        return "Hello World!";
+    }
+
+}
