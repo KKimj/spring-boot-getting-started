3fdc426c1e3dd55bfa3d0f9a275bd5285cda823b
Jiun Kim <kkimj@hanyang.ac.kr>
2023-01-22 20:57:55 +0900

    Update to add `Hello World Gradle`

---
+++ projects/helloworld_gradle/src/main/java/com/kkimj/helloworld_gradle/HelloworldGradleApplication.java
@@ -0,0 +1,13 @@
+package com.kkimj.helloworld_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldGradleApplication.class, args);
+	}
+
+}
---
+++ projects/helloworld_gradle/src/test/java/com/kkimj/helloworld_gradle/HelloworldGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkimj.helloworld_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
